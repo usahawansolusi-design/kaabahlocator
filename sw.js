fb23cf9993409670a92be870a6abb9fa81106daf
sharizan shapiin <usahawansolusi@gmail.com>
2026-07-02 09:58:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,40 @@
-const CACHE_NAME = 'kaabah-finder-v8';
+const CACHE_NAME = 'kaabah-finder-v9';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
 ];
 
+// Pasang Service Worker & Simpan Fail dalam Cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting())
   );
 });
 
+// Aktifkan & Bersihkan Cache Lama
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Ambil data dari Cache jika offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    }).catch(() => caches.match('./index.html'))
   );
 });
